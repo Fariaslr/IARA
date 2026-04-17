4ff6a72af9d789e3bafa31810d7df6113965f437
Lucas Farias <lucasfarias485@gmail.com>
2026-04-16 22:52:21 -0300

    :hammer: Implement agent in Twillio

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from agno.agent import Agent
-from dotenv import load_dotenv
 from agno.models.google import Gemini
+from dotenv import load_dotenv
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 load_dotenv()
 
+app = Flask(__name__)
+
 agent = Agent(
-    id = "Atendente Humanizado",
-    name = "Atendente de mensagem no Whatsapps",
-    role = "Você tem a responsabilidade de atender os clientes de maneira humanizada e simples",
-     instructions="""
+    id="atendente-humanizado",
+    name="Atendente de mensagem no WhatsApp",
+    role="Você tem a responsabilidade de atender os clientes de maneira humanizada e simples",
+    instructions="""
     Você é um atendente de uma bomboniere.
 
     Contexto:
@@ -27,6 +31,18 @@ agent = Agent(
     - Sugerir produtos
     - Conduzir para uma compra
     """,
-    model=Gemini("gemini-2.5-flash-lite"))
-    
-print(agent.print_response("Boa tarde, como vai? Estou interessado em um bombom que possui pouco açúcar!"))
+    model=Gemini(id="gemini-2.5-flash-lite"),
+)
+
+@app.route("/whatsapp", methods=["POST"])
+def whatsapp():
+    mensagem_usuario = request.form.get("Body")
+    resposta_ia = agent.run(mensagem_usuario).content
+
+    resp = MessagingResponse()
+    resp.message(resposta_ia)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(port=3000)
